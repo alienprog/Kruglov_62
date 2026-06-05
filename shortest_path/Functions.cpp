a64b77abd8f838f211f083299693f30f8dec52f2
Maxim <maks-kruglov-05@mail.ru>
2026-06-05 18:21:12 +0300

    Добавил Functions.cpp и shortest_path.cpp с заглушками

---
+++ shortest_path/Functions.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+
+void parseMazeContent(const std::string& content, Maze& maze, std::set<Error>& errors) {
+}
+
+bool parseTwoInts(const std::string& line, int lineNum, int& a, int& b, std::set<Error>& errors) {
+    return false;
+}
+
+bool parseCoordsContent(const std::string& content, const Maze& maze, int& startRow, int& startCol, int& endRow, int& endCol, std::set<Error>& errors) {
+    return false;
+}
--- shortest_path/shortest_path.cpp
+++ shortest_path/shortest_path.cpp
@@ -1,20 +1,18 @@
-﻿// shortest_path.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include "Header.h"
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+void readMazeFile(const std::string& filename, std::string& content, std::set<Error>& errors) {
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void readCoordsFile(const std::string& filename, std::string& content, std::set<Error>& errors) {
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+bool writeResult(const std::string& filename, const std::vector<Cell*>& path) {
+    return false;
+}
+
+void printErrors(const std::set<Error>& errors) {
+}
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
