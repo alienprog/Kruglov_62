dbc402b4d1b53a7132c4d6c2aa0f525c458c8496
Maxim <maks-kruglov-05@mail.ru>
2026-06-07 22:13:14 +0300

    Реализовал конструкторы и все методы класса Maze

--- shortest_path/Maze.cpp
+++ shortest_path/Maze.cpp
@@ -1,25 +1,30 @@
 #include "Maze.h"
 
-Maze::Maze()
-    : rows(0),
-    cols(0) {
-}
+Maze::Maze() : rows(0), cols(0) {}
 
-Maze::Maze(int r, int c)
-    : rows(r),
-    cols(c),
-    grid(r, std::vector<Cell>(c)) {
+Maze::Maze(int r, int c) : rows(r), cols(c), grid(r, std::vector<Cell>(c)) {
+    for (int i = 0; i < r; ++i) {
+        for (int j = 0; j < c; ++j) {
+            grid[i][j].x = j;
+            grid[i][j].y = i;
+        }
+    }
 }
 
 bool Maze::isValidCell(int row, int col) const {
-    return false;
+    return row >= 0 && row < rows && col >= 0 && col < cols;
 }
 
 bool Maze::isPassable(int row, int col) const {
-    return false;
+    return isValidCell(row, col) && !grid[row][col].isWall;
 }
 
 void Maze::reset() {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            grid[i][j].reset();
+        }
+    }
 }
 
 Cell& Maze::getCell(int row, int col) {
