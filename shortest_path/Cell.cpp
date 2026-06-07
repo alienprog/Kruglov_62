1007f899a626ee0b2e6ab5375c759e79fa8b96ee
Maxim <maks-kruglov-05@mail.ru>
2026-06-07 16:48:50 +0300

    Реализовал конструктор и reset класса Cell

--- shortest_path/Cell.cpp
+++ shortest_path/Cell.cpp
@@ -1,6 +1,11 @@
 #include "Cell.h"
-
-Cell::Cell() {
+Cell::Cell()
+    : isWall(false), visited(false), distance(0),
+    parent(nullptr), x(0), y(0) {
 }
+
 void Cell::reset() {
+    visited = false;
+    distance = 0;
+    parent = nullptr;
 }
