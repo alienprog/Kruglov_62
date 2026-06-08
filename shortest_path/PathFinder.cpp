ae8c4ac1f5d418d9ee88dcca78b9d0876862f0c8
Maxim <maks-kruglov-05@mail.ru>
2026-06-08 21:37:58 +0300

    Реализовал BFS в findPath - обход четырёх соседних клеток

--- shortest_path/PathFinder.cpp
+++ shortest_path/PathFinder.cpp
@@ -1,12 +1,48 @@
 #include "PathFinder.h"
-PathFinder::PathFinder(Maze& m, int sr, int sc, int er, int ec)
-    : maze(&m),
-    startRow(sr),
-    startCol(sc),
-    endRow(er),
-    endCol(ec) {
-}
+#include <queue>
 
+PathFinder::PathFinder(Maze& m, int sr, int sc, int er, int ec) : maze(&m), startRow(sr), startCol(sc), endRow(er), endCol(ec) {}
 std::vector<Cell*> PathFinder::findPath() {
+    maze->reset();
+
+    Cell& startCell = maze->getCell(startRow, startCol);
+    startCell.visited = true;
+    startCell.distance = 0;
+    startCell.parent = nullptr;
+
+    std::queue<Cell*> q;
+    q.push(&startCell);
+
+    bool found = false;
+
+    const int dx[] = { 0, 0, -1, 1 };
+    const int dy[] = { -1, 1, 0, 0 };
+
+    while (!q.empty() && !found) {
+        Cell* current = q.front();
+        q.pop();
+
+        if (current->x == endCol && current->y == endRow) {
+            found = true;
+        }
+        else {
+            for (int direction = 0; direction < 4 && !found; ++direction) {
+                int nextCol = current->x + dx[direction];
+                int nextRow = current->y + dy[direction];
+
+                if (maze->isValidCell(nextRow, nextCol)) {
+                    Cell& neighbor = maze->getCell(nextRow, nextCol);
+
+                    if (!neighbor.isWall && !neighbor.visited) {
+                        neighbor.visited = true;
+                        neighbor.distance = current->distance + 1;
+                        neighbor.parent = current;
+                        q.push(&neighbor);
+                    }
+                }
+            }
+        }
+    }
+
     return std::vector<Cell*>();
 }
