32d9d5b72cb9b10f7e18879e206e8156978e0d28
Maxim <maks-kruglov-05@mail.ru>
2026-06-10 20:27:49 +0300

    Реализовал readMazeFile и readCoordsFile

--- shortest_path/shortest_path.cpp
+++ shortest_path/shortest_path.cpp
@@ -1,9 +1,58 @@
 ﻿#include "Header.h"
+#include "PathFinder.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <set>
 
 void readMazeFile(const std::string& filename, std::string& content, std::set<Error>& errors) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        errors.insert(Error(fileOpenError));
+        return;
+    }
+
+    file.seekg(0, std::ios::end);
+
+    if (file.tellg() == 0) {
+        errors.insert(Error(fileEmptyError));
+        file.close();
+        return;
+    }
+
+    file.seekg(0, std::ios::beg);
+
+    std::ostringstream ss;
+    ss << file.rdbuf();
+    content = ss.str();
+
+    file.close();
 }
 
 void readCoordsFile(const std::string& filename, std::string& content, std::set<Error>& errors) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {  
+        errors.insert(Error(fileOpenError));
+        return;
+    }
+
+    file.seekg(0, std::ios::end);
+
+    if (file.tellg() == 0) {
+        errors.insert(Error(fileEmptyError));
+        file.close();
+        return;
+    }
+
+    file.seekg(0, std::ios::beg);
+
+    std::ostringstream ss;
+    ss << file.rdbuf();
+    content = ss.str();
+
+    file.close();
 }
 
 bool writeResult(const std::string& filename, const std::vector<Cell*>& path) {
