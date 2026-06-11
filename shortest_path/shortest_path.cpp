703836c5a3d9d46a884c1dba851948fd5686c2d4
Maxim <maks-kruglov-05@mail.ru>
2026-06-11 11:43:20 +0300

    Реализовал writeResult и printErrors

--- shortest_path/shortest_path.cpp
+++ shortest_path/shortest_path.cpp
@@ -56,10 +56,33 @@ void readCoordsFile(const std::string& filename, std::string& content, std::set<
 }
 
 bool writeResult(const std::string& filename, const std::vector<Cell*>& path) {
-    return false;
+    std::ofstream out(filename);
+
+    if (!out.is_open()) {
+        return false;
+    }
+
+    if (path.empty()) {
+        out << "No path found";
+    }
+    else {
+        for (std::size_t i = 0; i < path.size(); ++i) {
+            if (i > 0) {
+                out << ", ";
+            }
+
+            out << "(" << path[i]->y << ";" << path[i]->x << ")";
+        }
+    }
+
+    out.close();
+    return true;
 }
 
 void printErrors(const std::set<Error>& errors) {
+    for (const auto& error : errors) {
+        std::cerr << error.generate_error_message() << std::endl;
+    }
 }
 
 int main(int argc, char* argv[]) {
